571f49a2a0b0746e87e175c2736ddff7f990b8ba
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-01-17 16:58:41 -0500

    fix: svg fill and stroke are now set with the theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="h-7 w-7 fill-inherit stroke-current"
+            className="h-7 w-7 fill-current stroke-current"
           >
             <path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path>
             <path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path>
